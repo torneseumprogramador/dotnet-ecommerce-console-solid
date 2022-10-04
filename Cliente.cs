fae402ca9ecf4a95061e2cd23e876ab9c5c27f97
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2022-10-04 11:42:11 -0300

    codigo sem principios

--- Cliente.cs
+++ Cliente.cs
@@ -5,15 +5,21 @@ namespace ecommerce
 {
     class Cliente
     {
+        #region Constantes
+        private const string ARQUIVO_JSON = "clientes.json";
+        private const string ARQUIVO_CSV = "clientes.csv";
+        private const string COLUNAS_CSV = "id;nome;email;telefone;enderecoCompleto";
+        #endregion
+
         public int Id { get; set; }
         public string Nome { get; set; }
         public string Email { get; set; }
         public string Telefone { get; set; }
         public string EnderecoCompleto { get; set; }
 
-        public void Salvar()
+        public void Salvar(Tipo tipo = Tipo.Json)
         {
-            var clientes = Cliente.Todos();
+            var clientes = Cliente.Todos(tipo);
             var clienteExistente = clientes.Find(c => c.Id == this.Id);
             if(clienteExistente != null)
             {
@@ -22,19 +28,63 @@ namespace ecommerce
 
             clientes.Add(this);
 
-            var clientesJson = JsonConvert.SerializeObject(clientes);
-            File.WriteAllText(ARQUIVO, clientesJson);
+            if(tipo == Tipo.Json){
+                var clientesJson = JsonConvert.SerializeObject(clientes);
+                File.WriteAllText(ARQUIVO_JSON, clientesJson);
+            }
+            else{
+                var linhas = COLUNAS_CSV + "\n";
+                foreach(var cliente in clientes){
+                    linhas += $"{cliente.Id};{cliente.Nome};{cliente.Email};{cliente.Telefone};{cliente.EnderecoCompleto}\n";
+                }
+
+                File.WriteAllText(ARQUIVO_CSV, linhas);
+            }
         }
 
-        private const string ARQUIVO = "clientes.json";
-
-        public static List<Cliente> Todos()
+        public static List<Cliente> Todos(Tipo tipo = Tipo.Json)
         {
-            if(!File.Exists(ARQUIVO)) File.WriteAllText(ARQUIVO, "[]");
-            List<Cliente> clientes = new List<Cliente>();
-            var clientesJson = File.ReadAllText(ARQUIVO);
-            clientes = JsonConvert.DeserializeObject<List<Cliente>>(clientesJson);
-            return clientes;
+            if(tipo == Tipo.Json)
+            {
+                if(!File.Exists(ARQUIVO_JSON)) File.WriteAllText(ARQUIVO_JSON, "[]");
+                List<Cliente> clientes = new List<Cliente>();
+                var clientesJson = File.ReadAllText(ARQUIVO_JSON);
+                clientes = JsonConvert.DeserializeObject<List<Cliente>>(clientesJson);
+                return clientes;
+            }
+            else{
+                if(!File.Exists(ARQUIVO_CSV)) File.WriteAllText(ARQUIVO_CSV, COLUNAS_CSV);
+                List<Cliente> clientes = new List<Cliente>();
+
+                string text = File.ReadAllText(ARQUIVO_CSV);
+                string[] lines = text.Split(Environment.NewLine);
+        
+                foreach (string line in lines)
+                {
+                    var coluns = line.Split(';');
+                    if(coluns[0].Trim().ToLower() == "id" || coluns[0].Trim().ToLower() == "") continue;
+
+                    clientes.Add(new Cliente(){
+                        Id = int.Parse(coluns[0]),
+                        Nome = coluns[1],
+                        Email = coluns[2],
+                        Telefone = coluns[3],
+                        EnderecoCompleto = coluns[4]
+                    });
+                }
+                
+                return clientes;
+            }
+
         }
+    }
+
+    enum Tipo
+    {
+        CSV,
+        Json,
+        SqlServer,
+        MySql,
+        Mongodb
     }
 }
--- Program.cs
+++ Program.cs
@@ -5,13 +5,36 @@
         static void Main(string[] args)
         {
             new Cliente(){
+                Id = 1,
+                Nome = "Leandro",
+                Email = "leandro@teste.com",
+                EnderecoCompleto = "Rua teste 123 SP"
+            }.Salvar(Tipo.CSV);
+
+            new Cliente(){
                 Id = 2,
                 Nome = "Danilo",
                 Email = "danilo@teste.com",
                 EnderecoCompleto = "Rua teste 123 SP"
-            }.Salvar();
+            }.Salvar(Tipo.CSV);
 
             Console.WriteLine("O cliente foi salvo com sucesso!");
+
+            foreach(var cliente in Cliente.Todos())
+            {
+                Console.WriteLine("===============================");
+                Console.WriteLine($"Id: {cliente.Id}");
+                Console.WriteLine($"Nome: {cliente.Nome}");
+            }
+
+            Console.WriteLine("============[Lendo do CSV]===================");
+            foreach(var cliente in Cliente.Todos(Tipo.CSV))
+            {
+                Console.WriteLine("===============================");
+                Console.WriteLine($"Id: {cliente.Id}");
+                Console.WriteLine($"Nome: {cliente.Nome}");
+            }
+
         }
     }
 }
