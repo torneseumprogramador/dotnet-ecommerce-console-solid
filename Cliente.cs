df2bb5b4af70e4f57763e39545cb9fd98ee6f4eb
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2022-10-04 10:34:58 -0300

    iniciando projeto

---
+++ Cliente.cs
@@ -0,0 +1,39 @@
+using System;
+using Newtonsoft.Json;
+
+namespace ecommerce
+{
+    class Cliente
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Telefone { get; set; }
+        public string EnderecoCompleto { get; set; }
+
+        public void Salvar()
+        {
+            var clientes = Cliente.Todos();
+            var clienteExistente = clientes.Find(c => c.Id == this.Id);
+            if(clienteExistente != null)
+            {
+                clientes.Remove(clienteExistente);
+            }
+
+            clientes.Add(this);
+
+            var clientesJson = JsonConvert.SerializeObject(clientes);
+            File.WriteAllText(ARQUIVO, clientesJson);
+        }
+
+        private const string ARQUIVO = "clientes.json";
+
+        public static List<Cliente> Todos()
+        {
+            List<Cliente> clientes = new List<Cliente>();
+            var clientesJson = File.ReadAllText(ARQUIVO);
+            clientes = JsonConvert.DeserializeObject<List<Cliente>>(clientesJson);
+            return clientes;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ecommerce
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Cliente(){
+                Id = 2,
+                Nome = "Danilo",
+                Email = "danilo@teste.com",
+                EnderecoCompleto = "Rua teste 123 SP"
+            }.Salvar();
+
+            Console.WriteLine("O cliente foi salvo com sucesso!");
+        }
+    }
+}
